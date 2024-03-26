44615c03be744767fd041ecc85da9bd6b0109fd9
osamahamdi29 <164712904+osamahamdi29@users.noreply.github.com>
2024-03-26 04:24:40 +0200

    Add files via upload

---
+++ libraries/BlockAdBlock/index.php
@@ -0,0 +1,12 @@
+<?php
+
+    /*!
+	 * POCKET v4
+	 *
+	 * http://www.droidoxy.com
+	 * support@droidoxy.com
+	 *
+	 * Copyright 2020 DroidOXY ( http://www.droidoxy.com )
+	 */
+	 
+	 header("Location: /");
